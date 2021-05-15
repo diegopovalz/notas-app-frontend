e742dfe08be44948de8922b3dc060b1824433f4b
Diego <alejopov@gmail.com>
2021-05-15 18:13:25 -0500

    Added api url as environment variable instead of service variable

--- src/app/feature/notas/shared/nota.service.ts
+++ src/app/feature/notas/shared/nota.service.ts
@@ -2,26 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Nota } from './nota.model';
 import { Observable } from 'rxjs';
+import { environment as env } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotaService {
 
-  private baseUrl: string = 'http://localhost:8080/notas'
-
   constructor(private http: HttpClient) { }
 
   darNotas(): Observable<Nota[]> {
-    return this.http.get<Nota[]>(`${this.baseUrl}/`)
+    return this.http.get<Nota[]>(`${env.apiUrl}/`)
   }
 
   darNota(id: number): Observable<Nota> {
-    return this.http.get<Nota>(`${this.baseUrl}/${id}`)
+    return this.http.get<Nota>(`${env.apiUrl}/${id}`)
   }
 
   nuevaNota(nota: Nota): Observable<Nota> {
-    return this.http.post<Nota>(`${this.baseUrl}/nota`, nota)
+    return this.http.post<Nota>(`${env.apiUrl}/nota`, nota)
   }
 
 
