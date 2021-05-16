dcad15bbd78b858f11df571b37921210729a7da9
Diego <alejopov@gmail.com>
2021-05-16 15:04:53 -0500

    Centered alert

--- src/app/feature/notas/notas.component.ts
+++ src/app/feature/notas/notas.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Nota } from './shared/nota.model';
+import { NotaService } from './shared/nota.service';
 
 @Component({
   selector: 'app-notas',
@@ -7,13 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotasComponent implements OnInit {
 
-  public titulo: string = 'Prueba'
-  public mensaje: string = 'Prueba de Alerta'
-  public exito: boolean = true;
+  public titulo: string
+  public mensaje: string
+  public exito: boolean = false;
+  public notas: Nota[]
 
-  constructor() { }
+  constructor(private servicioNota: NotaService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    //this.conseguirNotas()
+    console.log('it works!')
+    this.exito = true;
+    this.titulo = 'Éxito'
+    this.mensaje = 'Sí se pudieron conseguir las notas'
+  }
+
+  public conseguirNotas(): void {
+    this.servicioNota.darNotas().subscribe((res: Nota[]) => {
+      this.notas = res
+      this.exito = true;
+      this.titulo = 'Éxito'
+      this.mensaje = 'Sí se pudieron conseguir las notas'
+    }), (error: HttpErrorResponse) => {
+      alert(error)
+      this.titulo = 'Error'
+      this.mensaje = 'No se pudieron conseguir las notas'
+    }
   }
 
 }
